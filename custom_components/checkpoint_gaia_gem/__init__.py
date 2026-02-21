cb848366adbb446abd41980fc613080d6f2470ed
deangoldhill <deangoldhill@gmail.com>
2026-02-21 18:47:42 +0000

    Update __init__.py

--- custom_components/checkpoint_gaia_gem/__init__.py
+++ custom_components/checkpoint_gaia_gem/__init__.py
@@ -1,13 +1,18 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
+DOMAIN = "checkpoint_gaia_gem"
+PLATFORMS = ["sensor"]
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up from a config entry."""
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the integration from configuration.yaml (legacy)."""
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the integration from a UI config entry."""
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
